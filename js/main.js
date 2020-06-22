be670b0195e9a4e4550677a4cc118314ff018a65
kafui4k <kalordo7@gmail.com>
2020-06-22 06:02:58 +0000

    Adding new Cards via Forms

--- js/main.js
+++ js/main.js
@@ -53,9 +53,48 @@ var myDatabase = [
     }
 
     var enterUser = function () {
-        console.log('form')
+
+        function grabUser() {
+            var name = document.querySelector('#user_name').value;
+            var email = document.querySelector('#user_email').value;
+            var age = document.querySelector('#user_age').value;
+
+            var elements = [name, email, age];
+            
+            if (validateUser(elements)) {
+                // add the card
+                document.querySelector('#myForm').reset();
+                db.push({name:name, email:email, age:age});
+                generateList();
+
+            } else {
+                // display error log
+                document.querySelector('#error').style.display = 'block';
+
+                setTimeout(function(){
+                    document.querySelector('#error').style.display = 'none';
+                }, 2000);
+            }
+        }
+
+
+        document.querySelector('#myForm').addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            grabUser();
+        })
+    }
+
+    var validateUser = function (elements) {
+        for (var i = 0; i < elements.length; i++) {
+            if (elements[i] == "") {
+                return false
+            }
+        }
+        return true
     }
 
     init();
+
 
 })(myDatabase)
